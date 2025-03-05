663455e50f72a8411a6ed3b7d71a26ed6de760ad
bernardchri <bernardchri@gmail.com>
2025-03-05 13:25:31 +0100

    update marqee

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => 'f936724c7b7569670367');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '6d6bb0aab7a185f6e3b2');
