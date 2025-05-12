3d63240d19ffba3a4e6b87945048537dc5d002b2
bernardchri <bernardchri@gmail.com>
2025-05-12 10:52:32 +0200

    build propre

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '4891da99a1de3fa196f0');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '14e3dfb3bf3b05c90cb7');
