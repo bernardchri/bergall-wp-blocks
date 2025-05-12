4e0d8f9f8454e05406ebb16afe6733012e9925a3
bernardchri <bernardchri@gmail.com>
2025-05-12 10:52:08 +0200

    reglage de conflits

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '3e11058288a600a3a998');
-=======
 <?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '4891da99a1de3fa196f0');
->>>>>>> f7eb55dec51038831f6661ccaf0ddb8c981001e8
