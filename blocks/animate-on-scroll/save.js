15b6a0a35adc0a371b7564096a0931006016742e
bernardchri <bernardchri@gmail.com>
2025-09-29 15:07:18 +0200

    update animate on scroll

---
+++ blocks/animate-on-scroll/save.js
@@ -0,0 +1,46 @@
+import { InnerBlocks } from '@wordpress/block-editor';
+
+export default function Save({ attributes }) {
+    const {
+        initialScale,
+        finalScale,
+        zIndex,
+        startX,
+        startY,
+        endX,
+        endY,
+        startRotate,
+        endRotate,
+        startOpacity,
+        endOpacity,
+        scrollEnterElement,
+        scrollEnterViewport,
+        scrollLeaveElement,
+        scrollLeaveViewport,
+        debug,
+    } = attributes;
+
+    return (
+        <div
+            className="animate-on-scroll"
+            data-initial-scale={initialScale}
+            data-final-scale={finalScale}
+            data-z-index={zIndex}
+            data-start-x={startX}
+            data-start-y={startY}
+            data-end-x={endX}
+            data-end-y={endY}
+            data-start-rotate={startRotate}
+            data-end-rotate={endRotate}
+            data-start-opacity={startOpacity}
+            data-end-opacity={endOpacity}
+            data-scroll-enter-element={scrollEnterElement}
+            data-scroll-enter-viewport={scrollEnterViewport}
+            data-scroll-leave-element={scrollLeaveElement}
+            data-scroll-leave-viewport={scrollLeaveViewport}
+            data-debug={debug ? 'true' : 'false'}
+        >
+            <InnerBlocks.Content />
+        </div>
+    );
+}
--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b291e4217f8e2417a338');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '319a29a043206d3478c7');
